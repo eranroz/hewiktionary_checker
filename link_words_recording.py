c9fb70c8bd57b1c8bda55ccb53180549ad352c95
kamomil <dafna3@gmail.com>
2018-01-18 18:51:25 +0200

    some fixes to link_words_recording.py

--- link_words_recording.py
+++ link_words_recording.py
@@ -54,41 +54,40 @@ class TEMPLATE_STATE:
     END = 3
     NEXT_PART = 4
 
-x = 0
 class HebrewWordsRecordsLinkerBot(pywikibot.CurrentPageBot):
 
     def treat_page(self):
-        global x
 
-        if x >= 20:
-            return
-        
         """Load the given page, do some changes, and save it."""
         #print(self.current_page.title())
-        s = re.compile(u'^File:He-([\u0590-\u05f4\u200f]+)\.ogg$').match(self.current_page.title())
+
+        #\u0590-\u05f4 is the hebrew unicode range
+        #\u200f is RIGHT_TO_LEFT mark
+        s = re.compile(u'^File:He-(.+)\.ogg$').match(self.current_page.title())
         if s:
             word = s.group(1) 
             site = pywikibot.Site('he','wiktionary')
-            word_without_nikud  = re.sub('[\u0590-\u05c7\u200f]','',word)
+            word_without_nikud  = re.sub('[\u0590-\u05c7\u05f0-\u05f4\u200f]','',word)
             #print("WITHOUT")
 
             wikt_page = pywikibot.Page(site,title = word_without_nikud)
-            #print(word)
+            print(word)
+            print(word_without_nikud)
             #
             if wikt_page.exists():
                 parts_gen = split_parts(wikt_page.text)
-                
+
                 # the first part will always be either an empty string or a string before the first definition (like {{לשכתוב}})
                 final = []
                 first  = parts_gen.__next__()[0]
 
                 if first.strip() != '':
                     final += [first]
-                state = TEMPLATE_STATE.BEFORE_START    
+                state = TEMPLATE_STATE.BEFORE_START
                 for part in parts_gen:
                     sec_word = re.compile("^==\s*([^=]+)\s*==\s*\n*").search(part[0]).group(1).strip()
                     sec_word = re.sub('\u200f','',sec_word)
-                    
+
                     if(sec_word == word):
                         final += [part[0]]
                         print("   FOUND MATCH: "+word)
@@ -103,13 +102,13 @@ class HebrewWordsRecordsLinkerBot(pywikibot.CurrentPageBot):
                             #verb_template_regex='\n*{{ניתוח\s+דקדוקי\sלפועל\s*\|?\s*'
                             #if line.strip('\n\r') != '':
                             final += [line+'\n']
-                            
+
                             if state == TEMPLATE_STATE.BEFORE_START and (re.compile(hewiktionary_constants.template_regex).search(line) or re.compile(hewiktionary_constants.verb_template_regex).search(line)):
                                 state = TEMPLATE_STATE.START
-                                
+
                             elif state == TEMPLATE_STATE.START:
                                 if re.search("}}",line) and not re.search("{{",line):
-                                    
+
                                     state = TEMPLATE_STATE.END
                                     break
                         if state != TEMPLATE_STATE.END:
@@ -122,11 +121,11 @@ class HebrewWordsRecordsLinkerBot(pywikibot.CurrentPageBot):
                         if state == TEMPLATE_STATE.END:
                             final += ['\n']
                             state = TEMPLATE_STATE.NEXT_PART
-                        
+
                         final += [part[0],part[1]]
 
                 new_page_text = ''.join(final)
-                x += 1
+
                 if new_page_text != wikt_page.text:
                     print('saving %s' % wikt_page.title())
                     wikt_page.text = new_page_text
@@ -134,11 +133,10 @@ class HebrewWordsRecordsLinkerBot(pywikibot.CurrentPageBot):
 
 
 def main(args):
-    
-    site = pywikibot.Site('commons', 'commons')    
+
+    site = pywikibot.Site('commons', 'commons')
     cat = pywikibot.Category(site,'Category:Hebrew_pronunciation')
     gen = pagegenerators.CategorizedPageGenerator(cat)
-    
     global_args  = []
 
     limit = 0
@@ -155,10 +153,10 @@ def main(args):
             global_args.append(arg)
 
     local_args = pywikibot.handle_args(global_args)
-#    if article:
-#        gen = [pywikibot.Page(site, article)]
-#        gen = pagegenerators.PreloadingGenerator(gen)
-        
+    if article:
+        gen = [pywikibot.Page(site, article)]
+        gen = pagegenerators.PreloadingGenerator(gen)
+
     bot = HebrewWordsRecordsLinkerBot(generator = gen)
     bot.run()  
 
