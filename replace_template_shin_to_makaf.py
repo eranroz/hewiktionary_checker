bb6ecb794b401e6a4ed052d20aec72c8883c740c
kamomil <dafna3@gmail.com>
2018-03-31 15:58:12 +0300

    stuff in replace_template_shin_to_makaf.py

--- replace_template_shin_to_makaf.py
+++ replace_template_shin_to_makaf.py
@@ -37,46 +37,43 @@ class ShinToMakafBot(pywikibot.CurrentPageBot):
 
 def main(args):
 
-    site = pywikibot.Site('he', 'wiktionary')
-    global_args  = []
-
-    limit = 0
-    article = ''
-
-    for arg in args:
-
-        m = re.compile('^-limit:([0-9]+)$').match(arg)
-        a = re.compile('^-article:(.+)$').match(arg)
-        if arg == '--get-dump':
-            get_dump()  # download the latest dump if it doesnt exist
-        elif m:
-            limit = int(m.group(1))
-        elif a:
-            article = a.group(1)
-        else:
-            global_args.append(arg)
-
     local_args = pywikibot.handle_args(global_args)
+    site = pywikibot.Site('he', 'wiktionary')
+    genFactory = pagegenerators.GeneratorFactory()
+    options = {}
 
-    #sys.exit(1)
+    parser = argparse.ArgumentParser(description=("a bot that replaces {{ש}} with {{-}}",epilog="Options include also global pywikibot options and all generators options", formatter_class=RawTextHelpFormatter))
 
-    if article != '':
+    parser.add_argument("--article",nargs=1, required=False)
+    parser.add_argument("-always",action='store_false', required=False,default=True)
+
+    args, factory_args = parser.parse_known_args(local_args)
+
+    options['always'] = args.always
+
+    for arg in factory_args:
+        genFactory.handleArg(arg)
+
+    genFactory.handleArg('-intersect')
+
+    gen = None
+
+    if args.article:
         print(article[::-1])
         gen = [pywikibot.Page(site, article)]
         gen = pagegenerators.PreloadingGenerator(gen)
-    elif os.path.exists('pages-articles.xml.bz2'):
+    elif os.path.exists('hewiktionary-latest-pages-articles.xml.bz2'):
         print('parsing dump')
-        all_wiktionary = XmlDump('pages-articles.xml.bz2').parse()  # open the dump and parse it.
+        all_wiktionary = XmlDump('hewiktionary-latest-pages-articles.xml.bz2').parse()  # open the dump and parse it.
         print('end parsing dump')
         # filter only main namespace
-        all_wiktionary = filter(lambda page: page.ns == '0' and not page.isredirect, all_wiktionary)
+        all_wiktionary = filter(lambda page: page.ns == '0' and not page.isredirect and page.title.endswith('(שורש)'), all_wiktionary)
         gen = (pywikibot.Page(site, p.title) for p in all_wiktionary)
         gen = pagegenerators.PreloadingGenerator(gen)
 
     else:
-        #TODO - make sure this case works
-        print('Not using dump - use get_dump to download dump file or run with comment lise arguments')
-        #gen_factory = pagegenerators.GeneratorFactory(site,'-ns:1')
+        if args.limit:
+        gen =  pagegenerators.AllpagesPageGenerator(site = site,includeredirects=False, total = limit)#only namespace 0 by default
         #gen_factory.getCombinedGenerator()
         gen =  pagegenerators.AllpagesPageGenerator(site = site,total = limit)
 
