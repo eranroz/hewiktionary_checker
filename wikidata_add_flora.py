e5ad76d2f07d9cd0ef55fa3aab8e2ddc8845a930
Dafna Hirschfeld <dafna3@gmail.com>
2018-08-06 16:45:33 +0300

    improve wikidata_add_flora.py

--- wikidata_add_flora.py
+++ wikidata_add_flora.py
@@ -1,3 +1,4 @@
+import os
 import re
 import pywikibot
 import json
@@ -6,7 +7,11 @@ import pickle
 from pywikibot import pagegenerators as pg
 from itertools import chain
 
-QUERY = """
+wikidata_site = pywikibot.Site("wikidata", "wikidata")
+repo = wikidata_site.data_repository()
+
+
+FAMILY_QUERY = """
 SELECT ?item ?desc
 WHERE
 {
@@ -18,21 +23,38 @@ WHERE
 }
 """
 
-wikidata_site = pywikibot.Site("wikidata", "wikidata")
+QGEN = """
+SELECT ?item ?desc
+WHERE
+{
+  ?item wdt:P105 wd:Q34740 .
+  ?item schema:description ?desc .
+  FILTER regex(?desc, "(plants|conifers)", "i")
+  filter (lang(?desc) = "en") .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+"""
+
+def get_query_output(pkl_file,query):
+    if os.path.isfile(pkl_file):
+        output = pickle.load(open(pkl_file, 'rb'))
+
+    else:
+        output = set()
+        generator = pg.WikidataSPARQLPageGenerator(query, site=wikidata_site)
+        for p in generator:
+            print(p)
+            p.get()
+            output.add(p)
+            print(p.aliases)
+        pickle.dump(output, open(pkl_file, "wb"))
+    return output
 
 
-try:
-    plants = pickle.load(open('plants.pkl','rb'))
 
-except Exception:
-    plants = set()
-    generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
-    for p in generator:
-        print(p)
-        p.get()
-        plants.add(p)
-        print(p.aliases)
-    pickle.dump(plants, open("plants.pkl", "wb"))
+wiki_families = get_query_output('wiki_families.pkl', FAMILY_QUERY)
+wiki_genuses = get_query_output('wiki_genuses.pkl', QGEN)
+
 
 """
 [
@@ -57,11 +79,20 @@ spelling = {'Lytheraceae' : 'Lythraceae',
             'Simarubiaceae':'Simaroubaceae',
             'Gymnogrammaceae': 'Pteridaceae'}
 
+
+errs_file = open('errors.txt','w')
+
+wiki_families = set(filter(lambda p: 'he' not in p.labels,wiki_families))
+pickle.dump(wiki_families, open("wiki_families.pkl", "wb"))
+
+wiki_genuses = set(filter(lambda g: 'he' not in g.labels,wiki_genuses))
+pickle.dump(wiki_genuses, open("wiki_genuses.pkl", "wb"))
+
 with open('all.json') as f:
     data = json.load(f)
-    for family in data:
-        found  = False
-        lat = family['lat_name']
+    for flora_il_family in data:
+
+        lat = flora_il_family['lat_name']
         if lat in spelling:
             lat = spelling[lat]
         if '(' in lat:
@@ -70,17 +101,40 @@ with open('all.json') as f:
             lat2 = m.group(2)
         else:
             lat1 = lat2 = lat
-        for plant in plants:
-            alias_vals = plant.aliases.values()
+        for wiki_family in wiki_families:
+            alias_vals = wiki_family.aliases.values()
             alias_list = set(chain.from_iterable(alias_vals))
-            if lat1 in alias_list or lat1 in plant.labels.values() \
-               or lat2 in alias_list or lat2 in plant.labels.values():
-                print("FOUND")
-                if 'he' not in plant.labels:
-                    plant.editLabels(labels={'he': family['heb_name']}, summary=u'Add hebrew label')
-                found = True
+            if lat1 in alias_list or lat1 in wiki_family.labels.values() \
+               or lat2 in alias_list or lat2 in wiki_family.labels.values():
+                current_family = pywikibot.ItemPage(repo, wiki_family.title())
+                current_family.get()
+                if 'he' not in current_family.labels:
+                    try:
+                        current_family.editLabels(labels={'he': flora_il_family['heb_name']}, summary=u'Add hebrew label')
+                    except pywikibot.OtherPageSaveError:
+                        print("already set")
+                found_fam = True
                 break
+        for flora_il_gen in flora_il_family["genuses"]:
+            lat = flora_il_gen['lat_name']
+            found = False
+            for wiki_genus in wiki_genuses:
+                alias_vals = wiki_genus.aliases.values()
+                alias_list = set(chain.from_iterable(alias_vals))
+                if lat in alias_list or lat in wiki_genus.labels.values():
+                    current_genus = pywikibot.ItemPage(repo, wiki_genus.title())
+                    current_genus.get()
+                    if 'he' not in current_genus.labels:
+                        try:
+                            current_genus.editLabels(labels={'he': flora_il_gen['heb_name']}, summary=u'Add hebrew label')
+                        except pywikibot.OtherPageSaveError:
+                            print("already set")
+                    found = True
+                    break
 
-        if not found:
-            print("NOT")
-            print("#%s#" % lat)
+            if not found:
+                errs_file.write("wiki genus not found #%s#\n" % lat)
+                errs_file.flush()
+                print("NOT")
+                print("#%s#" % lat)
+    errs_file.close()
